1a04f969b0f06badd1264d7ce23364f2771a6854
GevorgAnt <gev.antonyan.1999@gmail.com>
2024-01-28 14:55:50 +0400

    first commit

--- src/main/java/com/example/vedomost/config/SecurityConfig.java
+++ src/main/java/com/example/vedomost/config/SecurityConfig.java
@@ -3,31 +3,30 @@ package com.example.vedomost.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // Allow access to static resources
-                        .requestMatchers("/login").permitAll() // Allow access to login page
-                        .anyRequest().authenticated() // Require authentication for other requests
-                )
-                .formLogin()
-                .loginPage("/login") // Specify login page URL
-        // ... other form login configurations (usernameParameter, passwordParameter, etc.)
-        // ... other security configurations as needed
-        ;
-        return http.build();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll())
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/css/**").permitAll()
+                                .anyRequest().authenticated())
+                .build();
+
+
     }
 
 
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/vendor/**", "/fonts/**").anyRequest();
+    }
 }
